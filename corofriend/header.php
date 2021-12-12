b2647d5c2dadb5831ecd6dc2495a266c92db4527
whoaiam1 <70317811+whoaiam1@users.noreply.github.com>
2021-12-12 23:15:32 +0700

    Update header.php

--- corofriend/header.php
+++ corofriend/header.php
@@ -10,39 +10,68 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap" rel="stylesheet">
     <style>
-        .separator {
-            width: 65%;
+        .nav-item > a{
+            color:#ECB365!important;
         }
-        #in {
-            width: 100%;
-            color: black;
+        .navbar-brand{
+            color:#ECB365!important;
         }
+        .custom-toggler .navbar-toggler-icon {
+            background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(236,179,101, 0.8)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+        }
+        .custom-toggler.navbar-toggler {
+            border-color: #ECB365!important;
+        } 
         .nav-item > a:hover {
-            color: white!important;
-            background-color: #FF2E35;
+            color: #041C32!important;
+            background-color: #ECB365;
+        }
+        .sticky-top{
+            background-color: #04293A;
+        }
+        .sticky-top.scrolled {
+            background-color: #041C32 !important;
+            transition: background-color 200ms linear;
         }
     </style>
+        <!-- script scroll -->
+<script>
+        $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".sticky-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > 500);
+        });
+        });
+</script>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavigation" aria-controls="myNavigation" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">Corofriend</a>
+            <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#myNavigation" aria-controls="myNavigation" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="myNavigation">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="quiz.php">Quiz</a></li>
                     <li class="nav-item"><a class="nav-link" href="leaderboard.php">Leaderboard</a></li>
                 </ul>
-                <div class="separator"></div>
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" id="in" href="login.php">Log In</a></li>
-                    <li class="nav-item"><a class="nav-link" id="in" href="registration.php">Register</a></li>
-                    <li class="nav-item"><a class="nav-link" id="in" href="logout.php" onclick="return confirm('Are you sure you want to log out ?');">Log Out</a></li>
+                <ul class="navbar-nav ml-auto">
+                    <?php
+                        if(isset($_SESSION["login"])){ 
+                        echo '<li class="nav-item"><a class="nav-link" href="logout.php" onclick="return confirm(\'Are you sure you want to log out ?\');">Log Out</a></li>';
+                        }
+                        else{ 
+                        echo '<li class="nav-item"><a class="nav-link" href="login.php">Log In</a></li>
+                        <li class="nav-item"><a class="nav-link" href="registration.php">Register</a></li>';
+                        }
+                    ?>
                 </ul>
             </div>
         </div>
