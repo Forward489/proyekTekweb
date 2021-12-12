e34818bb843df053169df07c61b2c1935e3e4bcc
whoaiam1 <70317811+whoaiam1@users.noreply.github.com>
2021-12-12 21:05:01 +0700

    sama seperti commit sblmnya

--- corofriend/registration.php
+++ corofriend/registration.php
@@ -29,10 +29,6 @@ if (isset($_POST["regist"])) {
         }
     </style>
 </head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <body>
     <?php require "header.php"; ?>
