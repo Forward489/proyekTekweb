778adea62efa7b428e5f139be972dd62c0e4a1af
Glenn Steven Santoso <89455580+Forward489@users.noreply.github.com>
2021-12-14 21:11:18 +0700

    Nambahi fungsi ganti password
    Bisa ganti password cuy

--- corofriend/functions.php
+++ corofriend/functions.php
@@ -41,4 +41,52 @@ function registration($value) {
     return mysqli_affected_rows($conn);
 }
 
+function changePass($value)
+{
+    global $conn;
+    $old_username = $value["old_username"];
+
+    $username = strtolower(stripslashes($value["username"]));
+    $password = mysqli_real_escape_string($conn, $value["password"]);
+
+    $password_new = mysqli_real_escape_string($conn, $value["password_new"]);
+    $confirm = mysqli_real_escape_string($conn, $value["password2"]);
+
+    $check = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+    $check2 = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+    $old_password = mysqli_query($conn, "SELECT password FROM user WHERE username = '$username'");
+    $old_password = mysqli_fetch_assoc($old_password);
+    $check2 = mysqli_fetch_assoc($check2);
+
+    if (mysqli_fetch_assoc($check)) {
+        if (!$check2["username"] == $old_username) {
+            echo "<script>
+            alert('Username existed already !')
+            </script>";
+            return false;
+        }
+    }
+
+    if (!password_verify($password, $old_password["password"])) {
+        echo "<script>
+        alert('Old password doesn't match !')
+        </script>";
+        return false;
+    }
+
+    if ($password_new !== $confirm) {
+        echo "<script>
+        alert('New password does not match !')
+        </script>";
+        return false;
+    }
+
+    $password_new = password_hash($password_new, PASSWORD_DEFAULT);
+    
+    mysqli_query($conn, "UPDATE user SET username = '$username', password = '$password_new' WHERE username='$old_username'");
+
+    return mysqli_affected_rows($conn);
+}
+
+
 ?>
