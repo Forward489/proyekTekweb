788384cb1304e7735aab6621f5c65a9bd7333fd2
Glenn Steven Santoso <89455580+Forward489@users.noreply.github.com>
2021-12-16 10:22:20 +0700

    Corofriend dipencet jadi hof
    Corofriend dipencet jadi hof

--- corofriend/header.php
+++ corofriend/header.php
@@ -52,7 +52,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Corofriend</a>
+            <a class="navbar-brand" href="hallOfFame.php">Corofriend</a>
             <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#myNavigation" aria-controls="myNavigation" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
